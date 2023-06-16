8d8e7e947afb46320a484ca4e38d50984094e0ca
Isadora Dantas <isadoramariadasilvadantas@gmail.com>
2023-06-16 20:34:28 -0300

    First Commit

---
+++ lib/assets/javascripts/sigmun.d.ts
@@ -0,0 +1,12 @@
+const sigmun = {
+    functions: {
+        showAlert(message: string, type: string, container: string, timeout: number): void
+    }
+}
+
+
+// export as namespace
+export as namespace sigmun;
+
+// export as default
+export default sigmun;
---
+++ lib/sigmun_rubygems.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sigmun_rubygems/version"
+
+module SigmunRubygems
+  class Error < StandardError; end
+  # Your code goes here...
+end
